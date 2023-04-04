cfb65d661b8061025b235a799c9df9815bd21865
setak482 <yumsan1305@gmail.com>
2023-04-05 08:59:01 +0900

    다면체

---
+++ BOJ_Bronze/B3/10569.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a; cin >> a;
+    for(int i = 0; i < a; i++){
+        int a, b; cin >> a >> b;
+        cout << b-a+2 << '\n';
+    }
+}
